0443c9c7b3d5a5ac41775c7c1a83c74628878ad2
jreich <jonathan.reich@rubix.com.au>
2021-10-24 01:20:12 +1100

    update cmake file

--- src/random_uniform_generator.cpp
+++ src/random_uniform_generator.cpp
@@ -1,10 +1,21 @@
-#include "../include/random_uniform_generator.hpp"
-RandomNumberBetween::RandomNumberBetween(int low, int high)
+//#include "../include/random_uniform_generator.hpp"
+
+#include <random>
+
+class RandomNumberBetween
+{
+public:
+	RandomNumberBetween(int low, int high)
 		: random_engine_{ std::random_device{}() }
 		, distribution_{ low, high }
 	{
-	}
-int RandomNumberBetween::operator()()
+	}	
+	int RandomNumberBetween::operator()()
 	{
 		return distribution_(random_engine_);
 	}
+private:
+	std::mt19937 random_engine_;
+	std::uniform_int_distribution<int> distribution_;
+};
+
