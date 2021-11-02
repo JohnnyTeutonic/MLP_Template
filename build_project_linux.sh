a5375dd1cf5529c49b7f798df5f98bed32b518a7
jreich <jonathan.reich@rubix.com.au>
2021-11-02 15:20:48 +1100

    fix indentation on linux script file

--- build_project_linux.sh
+++ build_project_linux.sh
@@ -4,8 +4,8 @@ then
 	rm -rf build_linux
 fi
     echo "making build directory"
-	mkdir build_linux && cd build_linux
+    mkdir build_linux && cd build_linux
     echo "starting build process..."
-	cmake .. -G"Unix Makefiles"
-	make
-	echo "executable can be found in \"build_linux\" named updated_mlp"
+    cmake .. -G"Unix Makefiles"
+    make
+    echo "executable can be found in \"build_linux\" named updated_mlp"
